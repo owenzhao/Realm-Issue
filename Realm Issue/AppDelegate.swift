f550f53fa1e6b004216a5c837238811d19c2d617
肇鑫（Zhao Xin） <owenzx@gmail.com>
2020-07-21 15:39:29 +0800

    Initial Commit

---
+++ Realm Issue/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Realm Issue
+//
+//  Created by zhaoxin on 2020/7/21.
+//  Copyright © 2020 zhaoxin. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Realm Issue/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Realm Issue
+//
+//  Created by zhaoxin on 2020/7/21.
+//  Copyright © 2020 zhaoxin. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
